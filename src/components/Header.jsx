dabd4bb4a648bb7634d264100302c0c2de1dacab
Arthur <hayart98@gmail.com>
2021-01-26 17:33:46 +0400

    Log in pop up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'react'
 import LoginBtn from './LogInBtn'
+
 export default function Header(props) {
+    const [popUp, setPopUp] = useState(false)
+    function showPop() {
+        setPopUp(!popUp)
+    }
     return (
-        <div className="header">
-            {props.children}
-            <LoginBtn />
-        </div>
+        <>
+            <div className="header">
+                {props.children}
+                <LoginBtn />
+            </div>
+        </>
     )
 }
--- src/components/LogInBtn.jsx
+++ src/components/LogInBtn.jsx
@@ -1,3 +1,37 @@
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
+import { useState } from 'react'
+
 export default function LoginBtn(props) {
-    return <button className="btn loginBtn">Log in </button>
+    const { buttonLabel, className } = props
+
+    const [modal, setModal] = useState(false)
+
+    const toggle = () => setModal(!modal)
+
+    return (
+        <div>
+            <Button color="danger" onClick={toggle}>
+                login
+                {buttonLabel}
+            </Button>
+            <Modal isOpen={modal} toggle={toggle} className={className}>
+                <ModalHeader toggle={toggle}>Sing in</ModalHeader>
+                <ModalBody>
+                    <div className="login_form">
+                        <input placeholder="Your Email" type="mail" />
+                        <br />
+                        <input placeholder="Your Password" type="password" />
+                    </div>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" onClick={toggle}>
+                        Log in
+                    </Button>{' '}
+                    <Button color="secondary" onClick={toggle}>
+                        Cancel
+                    </Button>
+                </ModalFooter>
+            </Modal>
+        </div>
+    )
 }
